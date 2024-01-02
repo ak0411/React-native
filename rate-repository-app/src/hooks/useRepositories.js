11f1b44955dded5730e3c42389f166bde65b78be
Anawil Karavek <akaravek@hotmail.com>
2024-01-02 20:55:19 +0100

    done 10.11

---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,13 @@
+
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, loading, fetchMore } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network'
+  });
+
+  return { repositories: data && data.repositories, loading, refetch: fetchMore };
+};
+
+export default useRepositories;
